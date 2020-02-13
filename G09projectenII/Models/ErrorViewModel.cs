688c02cc6cb54ab81f242594a43342d5f9f80308
sam brysbaert <sam.brysbaert@student.hogent.be>
2020-02-13 11:19:33 +0100

    Add project files.

---
+++ G09projectenII/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace G09projectenII.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
