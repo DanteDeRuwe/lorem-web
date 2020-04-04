98007d6c8963112d27bf95978dc72fb744fc982f
sam brysbaert <sam.brysbaert@student.hogent.be>
2020-04-04 18:53:04 +0200

    finally able to add sessions to calendar view now using razor pages. super janky for now though -> needs cleanup

---
+++ G09projectenII/Pages/testing/Index.cshtml.cs
@@ -0,0 +1,25 @@
+﻿using G09projectenII.Models;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace G09projectenII.Views.testing
+{
+    public class IndexModel : PageModel
+    {
+        private readonly G09projectenII.Models.AppDbContext _context;
+
+        public IndexModel(G09projectenII.Models.AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Session> Sessions { get; set; }
+
+        public async Task OnGetAsync()
+        {
+            Sessions = await _context.Sessions.ToListAsync();
+        }
+    }
+}
