3e8133e4823ae3526554684df58ff8e5a436a94b
Arne De Schrijver <arne.deschrijver@student.hogent.be>
2020-04-22 18:42:54 +0200

    Wrote unit tests in SessionCalendarTest

---
+++ G09projectenII.Tests/Models/SessionCalendarTest.cs
@@ -0,0 +1,19 @@
+﻿using G09projectenII.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace G09projectenII.Tests.Models
+{
+    public class SessionCalendarTest
+    {
+        [Fact]
+        public void NewSessionCalendar_EmptyListOfSessions()
+        {
+            SessionCalendar sessionCalendar = new SessionCalendar();
+
+            Assert.Empty(sessionCalendar.Sessions);
+        }
+    }
+}
