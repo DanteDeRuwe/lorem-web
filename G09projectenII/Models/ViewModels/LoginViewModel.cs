0a3a8e16ab0a70ad13da7e41d9cdfa5dbad6ccd7
Spitaels Liam <liam.spitaels@student.hogent.be>
2020-04-24 16:41:59 +0200

    Add LoginViewModel

---
+++ G09projectenII/Models/ViewModels/LoginViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace G09projectenII.Models.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Display(Name = "Gebruikersnaam")]
+        [Required(ErrorMessage = "Gebruikersnaam is verplicht in te vullen!")]
+        public string Username { get; set; }
+
+        [Display(Name = "Paswoord")]
+        [Required(ErrorMessage = "Paswoord is verplicht in te vullen!")]
+        public string Password { get; set; }
+    }
+}
