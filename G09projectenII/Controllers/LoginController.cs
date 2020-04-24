92893432919d1ee1431b4bb55db19c1e9819861d
Spitaels Liam <liam.spitaels@student.hogent.be>
2020-04-24 17:20:35 +0200

    Add mostly login View and empty Controller

---
+++ G09projectenII/Controllers/LoginController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using G09projectenII.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace G09projectenII.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult LoginPage(LoginViewModel login)
+        {
+
+            return View(login);
+        }
+    }
+}
