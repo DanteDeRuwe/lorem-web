4b928c482d5d63e26c46f75b9f6ebb99dc70417f
Dante De Ruwe <dante.deruwe@student.hogent.be>
2020-04-25 15:33:42 +0200

    Changed time formatting + deleted an unnecessary js file

--- G09projectenII/wwwroot/js/calendar.js
+++
@@ -1,9 +0,0 @@
-﻿document.addEventListener('DOMContentLoaded', function () {
-    var calendarEl = document.getElementById('calendar');
-
-    var calendar = new FullCalendar.Calendar(calendarEl, {
-        plugins: ['dayGrid']
-    });
-
-    calendar.render();
-});
