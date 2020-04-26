187f64ccaeee839a7f5af1a1772b262ddd00f495
Dante De Ruwe <dante.deruwe@student.hogent.be>
2020-04-26 13:52:30 +0200

    Changed the url structure for admin pages
    /Admin/Sessions and /Admin/Attendances are the 2 admin pages now

---
+++ G09projectenII/Controllers/AdminController.cs
@@ -0,0 +1,29 @@
+﻿using G09projectenII.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace G09projectenII.Controllers
+{
+    public class AdminController : Controller
+    {
+        private readonly ISessionRepository _sessionRepository;
+
+        public AdminController(ISessionRepository sessionRepository) => _sessionRepository = sessionRepository;
+
+        public IActionResult Index() => RedirectToAction("Sessions");
+
+        public IActionResult Sessions()
+        {
+            List<Session> nonFinishedSessions =
+                _sessionRepository.GetAll()
+                    .Where(s => s.SessionState.toInt() != 3)
+                    .OrderBy(s => s.StartDateTime)
+                    .ToList();
+
+            return View(nonFinishedSessions);
+        }
+
+        public IActionResult Attendances() => View();
+    }
+}
