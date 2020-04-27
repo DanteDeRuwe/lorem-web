0b228c5e4dd3b983d4f901f70bcad2eff26adcd2
Spitaels Liam <liam.spitaels@student.hogent.be>
2020-04-27 16:11:05 +0200

    Made ProfilePicViewComponent properly reusable

--- G09projectenII/ViewComponents/ProfilePicViewComponent.cs
+++ G09projectenII/ViewComponents/ProfilePicViewComponent.cs
@@ -15,12 +15,20 @@ namespace G09projectenII.ViewComponents
             _memberRepository = memberRepository;
         }
 
-        public IViewComponentResult Invoke()
+        public IViewComponentResult Invoke(int width = 30, int height = 30, bool rounded = true, Member member = null)
         {
-            Member member = _memberRepository.GetAll().FirstOrDefault(m => m.Username == User.Identity.Name);
-            string profilePic = string.IsNullOrEmpty(member.Profilepicpath) ?
-                "https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png" : member.Profilepicpath;
-            return View(new ProfilePicViewModel { ProfilePicPath = profilePic });
+            // Use logged in member if no member is given
+            if (member == null) member = _memberRepository.GetAll().FirstOrDefault(m => m.Username == User.Identity.Name);
+
+            return View(new ProfilePicViewModel
+            {
+                Width = width,
+                Height = height,
+                Rounded = rounded,
+                // Use placeholder avatar if the member doesn't have a profile picture
+                ProfilePicPath = string.IsNullOrEmpty(member.Profilepicpath) ?
+                "https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png" : member.Profilepicpath
+            });
         }
     }
 }
