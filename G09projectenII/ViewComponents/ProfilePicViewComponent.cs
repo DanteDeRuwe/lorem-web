df9812db8892e3499a6b81bdf7f4cd2707f6ab02
Spitaels Liam <liam.spitaels@student.hogent.be>
2020-04-28 10:47:15 +0200

    Simplify the ProfilePicViewComponent a little

--- G09projectenII/ViewComponents/ProfilePicViewComponent.cs
+++ G09projectenII/ViewComponents/ProfilePicViewComponent.cs
@@ -15,15 +15,14 @@ namespace G09projectenII.ViewComponents
             _memberRepository = memberRepository;
         }
 
-        public IViewComponentResult Invoke(int width = 30, int height = 30, bool rounded = true, Member member = null)
+        public IViewComponentResult Invoke(int size = 32, bool rounded = true, Member member = null)
         {
             // Use logged in member if no member is given
             if (member == null) member = _memberRepository.GetAll().FirstOrDefault(m => m.Username == User.Identity.Name);
 
             return View(new ProfilePicViewModel
             {
-                Width = width,
-                Height = height,
+                Size = size,
                 Rounded = rounded,
                 // Use placeholder avatar if the member doesn't have a profile picture
                 ProfilePicPath = string.IsNullOrEmpty(member.Profilepicpath) ?
