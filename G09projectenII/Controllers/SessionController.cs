320cb3599b03bc1a49eef1693651911f55142e45
Dante De Ruwe <dante.deruwe@student.hogent.be>
2020-05-07 16:06:53 +0200

    Session detail screen!

--- G09projectenII/Controllers/SessionController.cs
+++ G09projectenII/Controllers/SessionController.cs
@@ -1,6 +1,7 @@
 ﻿using G09projectenII.Models;
 using G09projectenII.Models.Repository_Models;
 using Microsoft.AspNetCore.Mvc;
+using System.Linq;
 
 namespace G09projectenII.Controllers
 {
@@ -17,6 +18,20 @@ namespace G09projectenII.Controllers
 
 
         [Route("Session/{id}")]
-        public IActionResult Index(int id) => View(_sessionRepository.GetBy(id));
+        public IActionResult Index(int id)
+        {
+            var session = _sessionRepository.GetBy(id);
+            var member = _memberRepository.GetBy(HttpContext.User.Identity.Name);
+
+            if (session == null) return NotFound();
+
+            ViewBag.MemberIsRegistered = session.SessionRegistrees
+                .Any(r => r.MemberId == member.MemberId);
+
+            ViewBag.MemberHasAttended = session.SessionAttendees
+                .Any(a => a.MemberId == member.MemberId);
+
+            return View(session);
+        }
     }
 }
