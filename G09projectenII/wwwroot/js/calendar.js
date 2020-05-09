ab2f29d81758e1b95310fde1e9ec21eaa2f96bb8
sam brysbaert <sam.brysbaert@student.hogent.be>
2020-05-09 15:21:28 +0200

    formatting

--- G09projectenII/wwwroot/js/calendar.js
+++ G09projectenII/wwwroot/js/calendar.js
@@ -12,136 +12,137 @@ const loadLegend = () => {
 }
 
 
-
 function showSessionModal(session) {
-  // title
-  $("div.modal-header>.title").text(session.title);
+    // title
+    $("div.modal-header>.title").text(session.title);
 
-  // practical info
-  moment.locale("nl-be");
-  let startDateTime = moment(session["startDateTime"]);
-  let endDateTime = moment(session["endDateTime"]);
+    // practical info
+    moment.locale("nl-be");
+    let startDateTime = moment(session["startDateTime"]);
+    let endDateTime = moment(session["endDateTime"]);
 
-  $("div.modal-date>.info").text(startDateTime.format("dd D MMMM"));
+    $("div.modal-date>.info").text(startDateTime.format("dd D MMMM"));
 
-  $("div.modal-time>.info").text(
-    `${startDateTime.format("H:mm")} - ${endDateTime.format("H:mm")}`
-  );
-
-  $("div.modal-location>.info").text(session["location"]);
-
-  // description
-  if (session.description) {
-    $("div.modal-description>.description").text(session.description);
-    $("div.modal-description>.empty-description").empty();
-  } else {
-    $("div.modal-description>.empty-description").text(
-      "Geen beschrijving beschikbaar."
+    $("div.modal-time>.info").text(
+        `${startDateTime.format("H:mm")} - ${endDateTime.format("H:mm")}`
     );
-    $("div.modal-description>.description").empty();
-  }
-  
-  // organiser and speaker
-  $("div.modal-organiser>.info").text(session['organiser']['name']);
-  if (session['speakername']) {
-      $("div.modal-speaker").css("display", "inline-block");
-      $("div.modal-speaker>.info").text(session['speakername']);
 
-      $("div.modal-practical-info > div").removeClass("col-12 col-sm-4");
-      $("div.modal-practical-info > div").addClass("col-12 col-sm-6 col-lg-3");
+    $("div.modal-location>.info").text(session["location"]);
 
-  } else {
-      $("div.modal-speaker").css("display", "none");
+    // description
+    if (session.description) {
+        $("div.modal-description>.description").text(session.description);
+        $("div.modal-description>.empty-description").empty();
+    } else {
+        $("div.modal-description>.empty-description").text(
+            "Geen beschrijving beschikbaar."
+        );
+        $("div.modal-description>.description").empty();
+    }
 
-      $("div.modal-practical-info > div").removeClass("col-12 col-sm-6 col-lg-3");
-      $("div.modal-practical-info > div").addClass("col-12 col-sm-4");
-  }
-  
-  // announcement
-  let announcement = session["mostRecentAnnouncement"];
-  if (announcement) {
-    $("div.modal-announcement").css("display", "block");
-    
-    let author = announcement["author"];
-    let announcementDate = moment(announcement["timestamp"]);
-    
-    $("img.profile-picture").attr("src", author["profilepicpath"]);
-    $("div.organiser>.name").text(author["name"]);
-    $("div.announcement-date").text(announcementDate.format("DD/MM/YY"));
-    $("div.announcement-text").text(announcement['text']);
-  } else {
-    $("div.modal-announcement").css("display", "none");
-  }
+    // organiser and speaker
+    $("div.modal-organiser>.info").text(session['organiser']['name']);
+    if (session['speakername']) {
+        $("div.modal-speaker").css("display", "inline-block");
+        $("div.modal-speaker>.info").text(session['speakername']);
 
-  $.ajax({
-    type: "GET",
-    url: "/Calendar/GetRegistrationStatus?sessionId=" + session['id'],
-    contentType: "application/json; charset=utf-8",
-    dataType: "json",
-    success: fillFooter,
-    error: function() { alert('Server Error'); }
-  });
-  
-  function fillFooter(info) {
-    //See more
-    $("button.see-more-btn").click(() => location.href = "/Session/" + session["id"]);
-    
-      if (!info) {
-          if (session['isOpen']) {
-              $(".not-logged-in-register").css("visibility", "");
-              $("span.not-logged-in-session-free-spaces").text(`(nog ${session['availableRegistrationSpots']} vrije plaatsen)`);
-          } else {
-              $(".not-logged-in-register").css("visibility", "hidden");
-          }
-          return;
-      }
-
-    // Register
-    $(".not-logged-in-register").css("visibility", "hidden");
-    if (session['hasStarted']) {
-      $("div.register").css("display", "none");
-      $("div.attendance").css("display", "block");
-      if (info['hasAttended']) {
-        $("span.has-user-attended").html("<i class=\"fas fa-check\"></i> Aanwezig")
-            .addClass("badge-attended")
-            .removeClass("badge-not-attended");
-      } else {
-        $("span.has-user-attended").html("<i class=\"fas fa-times\"></i> Niet Aanwezig")
-            .addClass("badge-not-attended")
-            .removeClass("badge-attended");
-      }
-      $("span.session-attendances").text(`# Aanwezig: ${session['numberOfAttendees']}`);
+        $("div.modal-practical-info > div").removeClass("col-12 col-sm-4");
+        $("div.modal-practical-info > div").addClass("col-12 col-sm-6 col-lg-3");
 
     } else {
-      $("div.attendance").css("display", "none");
-      $("div.register").css("display", "flex");
-      if(info['isRegistered']) {
-        $("div.register button").text("Uitschrijven");
-      } else {
-          if (!session["isOpen"]) {
-              $("div.register button").prop('disabled', true);
-              $("div.register button").text(`Inschrijven nog niet mogelijk`);
-              $("span.session-free-spaces").css("display", "none");
-          } else {
-              $("span.session-free-spaces").css("display", "block");
-              $("div.register button").prop('disabled', false);
-              $("div.register button").text("Inschrijven");
-              $("span.session-free-spaces").text(`Nog ${session['availableRegistrationSpots']} vrije plaatsen`);
-          }
-      }
+        $("div.modal-speaker").css("display", "none");
 
-        $("div.register button").unbind('click');
-        $("div.register button").click(toggleRegistration)
-
-        function toggleRegistration() {
-            $.ajax({
-                type: "POST",
-                url: "/Session/ToggleRegistration?sessionId=" + session['id'],
-            });
-            $("#sessionModal .close").click();
+        $("div.modal-practical-info > div").removeClass("col-12 col-sm-6 col-lg-3");
+        $("div.modal-practical-info > div").addClass("col-12 col-sm-4");
     }
-  }
-}
+
+    // announcement
+    let announcement = session["mostRecentAnnouncement"];
+    if (announcement) {
+        $("div.modal-announcement").css("display", "block");
+
+        let author = announcement["author"];
+        let announcementDate = moment(announcement["timestamp"]);
+
+        $("img.profile-picture").attr("src", author["profilepicpath"]);
+        $("div.organiser>.name").text(author["name"]);
+        $("div.announcement-date").text(announcementDate.format("DD/MM/YY"));
+        $("div.announcement-text").text(announcement['text']);
+    } else {
+        $("div.modal-announcement").css("display", "none");
+    }
+
+    $.ajax({
+        type: "GET",
+        url: "/Calendar/GetRegistrationStatus?sessionId=" + session['id'],
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        success: fillFooter,
+        error: function () {
+            alert('Server Error');
+        }
+    });
+
+    function fillFooter(info) {
+        //See more
+        $("button.see-more-btn").click(() => location.href = "/Session/" + session["id"]);
+
+        if (!info) {
+            if (session['isOpen']) {
+                $(".not-logged-in-register").css("visibility", "");
+                $("span.not-logged-in-session-free-spaces").text(`(nog ${session['availableRegistrationSpots']} vrije plaatsen)`);
+            } else {
+                $(".not-logged-in-register").css("visibility", "hidden");
+            }
+            return;
+        }
+
+        // Register
+        $(".not-logged-in-register").css("visibility", "hidden");
+        if (session['hasStarted']) {
+            $("div.register").css("display", "none");
+            $("div.attendance").css("display", "block");
+            if (info['hasAttended']) {
+                $("span.has-user-attended").html("<i class=\"fas fa-check\"></i> Aanwezig")
+                    .addClass("badge-attended")
+                    .removeClass("badge-not-attended");
+            } else {
+                $("span.has-user-attended").html("<i class=\"fas fa-times\"></i> Niet Aanwezig")
+                    .addClass("badge-not-attended")
+                    .removeClass("badge-attended");
+            }
+            $("span.session-attendances").text(`# Aanwezig: ${session['numberOfAttendees']}`);
+
+        } else {
+            $("div.attendance").css("display", "none");
+            $("div.register").css("display", "flex");
+            if (info['isRegistered']) {
+                $("div.register button").text("Uitschrijven");
+            } else {
+                if (!session["isOpen"]) {
+                    $("div.register button").prop('disabled', true);
+                    $("div.register button").text(`Inschrijven nog niet mogelijk`);
+                    $("span.session-free-spaces").css("display", "none");
+                } else {
+                    $("span.session-free-spaces").css("display", "block");
+                    $("div.register button").prop('disabled', false);
+                    $("div.register button").text("Inschrijven");
+                    $("span.session-free-spaces").text(`Nog ${session['availableRegistrationSpots']} vrije plaatsen`);
+                }
+            }
+
+            $("div.register button").unbind('click');
+            $("div.register button").click(toggleRegistration)
+
+            function toggleRegistration() {
+                $.ajax({
+                    type: "POST",
+                    url: "/Session/ToggleRegistration?sessionId=" + session['id'],
+                });
+                $("#sessionModal .close").click();
+            }
+        }
+    }
 
     // show modal
     $("#sessionModal").modal();
