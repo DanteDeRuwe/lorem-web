2cfcd1c212982ddf0afe3aab2a9c6349b869dd26
sam brysbaert <sam.brysbaert@student.hogent.be>
2020-05-11 10:45:00 +0200

    disable register button if full

--- G09projectenII/wwwroot/js/calendar.js
+++ G09projectenII/wwwroot/js/calendar.js
@@ -139,7 +139,9 @@ function showSessionModal(session) {
                     $("span.session-free-spaces").css("display", "none");
                 } else {
                     $("span.session-free-spaces").css("display", "block");
-                    $("div.register button").prop('disabled', false);
+                    if(session['availableRegistrationSpots'] >= 0) {
+                        $("div.register button").prop('disabled', false);
+                    }
                     $("div.register button").text("Inschrijven");
                     $("span.session-free-spaces").text(`Nog ${session['availableRegistrationSpots']} vrije plaatsen`);
                 }
