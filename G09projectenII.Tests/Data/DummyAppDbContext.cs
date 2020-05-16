2b7c0e0ed683e382ab541e7875b1308507c0a95f
Arne De Schrijver <arne.deschrijver@student.hogent.be>
2020-05-16 21:34:27 +0200

    DummyAppDbContext created

---
+++ G09projectenII.Tests/Data/DummyAppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace G09projectenII.Tests.Data
+{
+    public class DummyAppDbContext : DbContext
+    {
+
+        #region Fields
+
+        #endregion
+
+        public DummyAppDbContext()
+        {
+
+        }
+
+    }
+}
