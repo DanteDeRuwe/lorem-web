d80fd88d2c85f30f5f6cca35972a752f82a95327
Arne De Schrijver <arne.deschrijver@student.hogent.be>
2020-05-17 21:15:23 +0200

    Added and adjusted tests in AccountController

--- G09projectenII.Tests/Controllers/AccountControllerTest.cs
+++ G09projectenII.Tests/Controllers/AccountControllerTest.cs
@@ -12,6 +12,9 @@ using Xunit;
 using Microsoft.AspNetCore.Mvc;
 using G09projectenII.Models;
 using G09projectenII.Models.ViewModels;
+using Microsoft.AspNetCore.Authentication;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
 
 namespace G09projectenII.Tests.Controllers
 {
@@ -19,20 +22,34 @@ namespace G09projectenII.Tests.Controllers
     {
 
         private readonly AccountController _accountController;
-        private readonly CalendarController _calendarController;
         private readonly DummyAppDbContext _dummyContext;
         private readonly Mock<IMemberRepository> _memberRepository;
-        private readonly Mock<ISessionRepository> _sessionRepository;
         private readonly Mock<HttpContext> _context;
+        private readonly Mock<IAuthenticationService> _aService;
+        private readonly Mock<IServiceProvider> _sProvider;
+        private readonly ControllerContext _controllerContext;
+        private readonly HttpContext _defaultContext;
 
         public AccountControllerTest()
         {
             _dummyContext = new DummyAppDbContext();
             _memberRepository = new Mock<IMemberRepository>();
-            _sessionRepository = new Mock<ISessionRepository>();
-            _accountController = new AccountController(_memberRepository.Object);
-            _calendarController = new CalendarController(_sessionRepository.Object, _memberRepository.Object);
+            _accountController = new AccountController(_memberRepository.Object)
+            {
+                TempData = new Mock<ITempDataDictionary>().Object,
+                Url = new Mock<IUrlHelper>().Object
+            };
             _context = new Mock<HttpContext>();
+            _aService = new Mock<IAuthenticationService>();
+            _sProvider = new Mock<IServiceProvider>();
+            _defaultContext = new DefaultHttpContext()
+            {
+                RequestServices = _sProvider.Object
+            };
+            _controllerContext = new ControllerContext()
+            {
+                HttpContext = _context.Object
+            };
         }
 
         #region --Index--
@@ -40,13 +57,17 @@ namespace G09projectenII.Tests.Controllers
         public void Index_UserIsAuthenticated_RedirectsToCalendarIndexView()
         {
             _context.Setup(u => u.User.Identity.IsAuthenticated).Returns(true);
-            var result = Assert.IsType<ViewResult>(_calendarController.Index());
+            _accountController.ControllerContext = _controllerContext;
+
+            var result = Assert.IsType<RedirectToActionResult>(_accountController.Index());
         }
 
         [Fact]
         public void Index_UserIsNotAuthenticated_GetsView()
         {
             _context.Setup(u => u.User.Identity.IsAuthenticated).Returns(false);
+            _accountController.ControllerContext = _controllerContext;
+
             var result = Assert.IsType<ViewResult>(_accountController.Index());
         }
         #endregion
@@ -56,13 +77,58 @@ namespace G09projectenII.Tests.Controllers
         public void Login_ValidLogin_RedirectsToCalendarIndexView()
         {
             _memberRepository.Setup(m => m.GetAll()).Returns(_dummyContext.Members);
+            _aService.Setup(_ => _.SignInAsync(It.IsAny<HttpContext>(), It.IsAny<String>(), It.IsAny<ClaimsPrincipal>(), It.IsAny<AuthenticationProperties>()))
+                     .Returns(Task.FromResult((object)null));
+            _sProvider.Setup(_ => _.GetService(typeof(IAuthenticationService)))
+                      .Returns(_aService.Object);
+            _accountController.ControllerContext = new ControllerContext()
+            {
+                HttpContext = _defaultContext
+            };
             var loginVM = new LoginViewModel()
             {
                 Username = "test",
                 Password = "test"
             };
+            
             var result = Assert.IsType<RedirectToActionResult>(_accountController.Login(loginVM));
             Assert.Equal("Index", result?.ActionName);
+        }
+
+        [Fact]
+        public void Login_NonValidLogin_ReturnsIndexView()
+        {
+            _memberRepository.Setup(m => m.GetAll()).Returns(_dummyContext.Members);
+            var loginVM = new LoginViewModel()
+            {
+                Username = "arne",
+                Password = "test"
+            };
+
+            var result = Assert.IsType<ViewResult>(_accountController.Login(loginVM));
+        }
+        #endregion
+
+        #region --Logout--
+        [Fact]
+        public void LogOut_RedirectsToCalendarIndexView()
+        {
+            _aService.Setup(_ => _.SignOutAsync(It.IsAny<HttpContext>(), It.IsAny<String>(), It.IsAny<AuthenticationProperties>())).Returns(Task.FromResult((object)null));
+            _sProvider.Setup(_ => _.GetService(typeof(IAuthenticationService))).Returns(_aService.Object);
+            _accountController.ControllerContext = new ControllerContext()
+            {
+                HttpContext = _defaultContext
+            };
+
+            var result = Assert.IsType<RedirectToActionResult>(_accountController.Logout());
+        }
+        #endregion
+
+        #region --AccessDenied--
+        [Fact]
+        public void AccessDenied_ReturnsAccessDeniedView()
+        {
+            var result = Assert.IsType<ViewResult>(_accountController.AccessDenied());
         }
         #endregion
     }
