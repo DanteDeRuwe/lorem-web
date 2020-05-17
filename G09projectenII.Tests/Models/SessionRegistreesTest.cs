d4645835b8624f7bb3d23eccf00075847e06281d
Arne De Schrijver <arne.deschrijver@student.hogent.be>
2020-05-17 02:20:30 +0200

    Added tests to SessionRegistreesTest

---
+++ G09projectenII.Tests/Models/SessionRegistreesTest.cs
@@ -0,0 +1,26 @@
+﻿using G09projectenII.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace G09projectenII.Tests.Models
+{
+    public class SessionRegistreesTest
+    {
+        [Fact]
+        public void NewSessionRegistreesWithParameters_InitiatesAllPropertiesCorrectly()
+        {
+            Session session = new Session();
+            session.Id = 1;
+            Member member = new Member();
+            member.MemberId = 1;
+            SessionRegistrees sr = new SessionRegistrees(session, member);
+
+            Assert.Equal(session, sr.Session);
+            Assert.Equal(1, sr.Id);
+            Assert.Equal(member, sr.Member);
+            Assert.Equal(1, sr.MemberId);
+        }
+    }
+}
